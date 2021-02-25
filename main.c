5957e079c2293c926dd8368a65976e863d68814e
notabelincoln <abejordandx@gmail.com>
2021-02-25 18:55:30 +0000

    Added error checking correctly, now has different for loops depending on the base

--- main.c
+++ main.c
@@ -15,33 +15,50 @@ int main(int argc, char **argv)
 		exit(-1);
 	}
 
-	// Check that input values are valid
-	for (i = 0; i < strlen(argv[1]); i++) {
-		if (argv[1][i] > '7' || argv[1][i] < '0') {
-			printf("ERROR: Invalid pin number\n");
-			exit(-2);
+	// Check if input pin mask is valid
+	if (!(strncmp("0x",argv[1],2))) {
+		if (strlen(argv[1]) < 3) {
+			printf("ERROR: Invalid pin mask\n");
+			exit(-1);
+		} else {
+			for (i = 2; i < strlen(argv[1]); i++) {
+				// Check if each character is not valid hex value (between '0' & '7', 'a' & 'f', 'A' & 'F')
+				if (!(((argv[1][i] <= '7') && (argv[1][i] >= '0'))
+							|| ((argv[1][i] <= 'f') && (argv[1][i] >= 'a'))
+							|| ((argv[1][i] <= 'F') && (argv[1][i] >= 'A')))) {
+					printf("ERROR: Invalid pin mask (hexadecimal)\n");
+					exit(-2);
+				}
+			}
+		}
+	} else if (argv[1][0] == '0') {
+		for (i = 0; i < strlen(argv[1]); i++) {
+			if (!((argv[1][i] <= '7') && (argv[1][i] >= '0'))) {
+				printf("ERROR: Invalid pin mask (base 8)\n");
+				exit(-2);
+			}
+		}
+	} else {
+		for (i = 0; i < strlen(argv[1]); i++) {
+			if (!((argv[1][i] <= '9') && (argv[1][i] >= '0'))) {
+				printf("ERROR: Invalid pin mask (base 10)\n");
+				exit(-2);
+			}
 		}
 	}
 
-	for (i = 0; i < strlen(argv[2]); i++) {
-		if ((argv[2][i] != '0') && (argv[2][i] != '1')) {
-			printf("ERROR: Invalid setting\n");
-			exit(-3);
-		}
-	}
-	
-	// Determine base of input pin
+
+	// Determine base of input pin mask
 	if (argv[1][0] == '0') {
-	       	if ((argv[1][1] == 'x') || (argv[1][1] == 'X'))
+		if ((argv[1][1] == 'x') || (argv[1][1] == 'X'))
 			pin_mask = strtol(argv[1], NULL, 16);
 		else
 			pin_mask = strtol(argv[1], NULL, 8);
 	} else {
-		pin_mask = atoi(argv[2]);
+		pin_mask = atoi(argv[1]);
 	}
 
 	wiringPiSetup();
-
 	pin_set = atoi(argv[2]);
 
 	for (i = 0; i < 8; i++) {
@@ -51,7 +68,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	while (1);
+	printf("pin_mask: %#4x\npin_set: %4u\n",pin_mask,pin_set);
 
 	return 0;
 }
